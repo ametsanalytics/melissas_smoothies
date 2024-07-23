3f257b90c905cd719016e3b0c399087b9f350096
MelissaBeeCodes <60261117+ametsanalytics@users.noreply.github.com>
2024-07-23 12:42:59 +0100

    Add descriptions and nutritional JSON values

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
 
 # Write directly to the app
 st.title("Customise Your Smoothie :cup_with_straw:")
@@ -12,8 +10,7 @@ st.write(
     """
 )
 
-#add name
-
+#add name and instruction
 title = st.text_input("Name on Smoothie: ", "")
 st.write("The name on your smoothie will be:", title)
 
@@ -21,30 +18,36 @@ st.write("The name on your smoothie will be:", title)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+# create dataframe for to use in ingredients list
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
+# create ingredients list
 ingredients_list = st.multiselect(
     "choose up to 5 ingredients: "
     , my_dataframe
     , max_selections=5
 )
+
+# create conditional loop for sending requests to database
 if ingredients_list:
-   # st.write(ingredients_list)
-   # st.text(ingredients_list)
-
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
-   # st.write(ingredients_string)
+# create variable for inserting data into orders table if not null
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + title + """')"""
     
-    # st.write(my_insert_stmt)
+# create variable for submit button
     time_to_insert = st.button("Submit to order")
+
+# submit order with insert staement to database when button is not null / selected
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered! '+ title, icon="✅")
+
+# display nutritional infomation from fruityvice
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
