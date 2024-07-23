37646d17ee9a2780a3c799d6cb51008bd2d5d199
MelissaBeeCodes <60261117+ametsanalytics@users.noreply.github.com>
2024-07-23 12:48:10 +0100

    add NI for Fruits chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 
 # Write directly to the app
@@ -31,8 +33,12 @@ ingredients_list = st.multiselect(
 # create conditional loop for sending requests to database
 if ingredients_list:
     ingredients_string = ''
+    
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+            ingredients_string += fruit_chosen + ' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
+
 # create variable for inserting data into orders table if not null
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -48,7 +54,3 @@ if ingredients_list:
         st.success('Your smoothie is ordered! '+ title, icon="✅")
 
 # display nutritional infomation from fruityvice
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
