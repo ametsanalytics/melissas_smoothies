8d41246dd0950c04eda0f66611591e3cec1f84a6
MelissaBeeCodes <60261117+ametsanalytics@users.noreply.github.com>
2024-07-23 14:03:49 +0100

    remove table

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # convert Snowpark datafram to pandas dataframe for loc function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 
 
 # create ingredients list
