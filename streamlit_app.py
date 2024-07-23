ee4124954210616a697fc370531d6586a3280c39
MelissaBeeCodes <60261117+ametsanalytics@users.noreply.github.com>
2024-07-23 13:41:00 +0100

    remove stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # convert Snowpark datafram to pandas dataframe for loc function
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop
+
 
 # create ingredients list
 ingredients_list = st.multiselect(
