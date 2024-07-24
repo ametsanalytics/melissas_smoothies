346f52ec8fde16fab64e8fc4764125d532575723
MelissaBeeCodes <60261117+ametsanalytics@users.noreply.github.com>
2024-07-24 09:36:28 +0100

    fix max selection error message
    added variable BEFORE multiselect widget for reference

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,12 +30,13 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 
+max_selections = 5
 
 # create ingredients list
 ingredients_list = st.multiselect(
     "choose up to 5 ingredients: "
     , my_dataframe
-    , max_selections=5
+    , max_selections=max_selections
 )
 
 # create conditional loop for sending requests to database
